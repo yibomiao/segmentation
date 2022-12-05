20a10d1f55b652783f831ca0e2f64d0ac4618d2e
instromica <45325713+instromica@users.noreply.github.com>
2022-12-05 09:17:56 +0800

    Add files via upload

---
+++ vqseg/vqseg.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$#" -eq  "0" ]
+   then
+     python3 reco_plus.py \
+     --p_config "configs/reco_plus_cityscapes.yaml"
+ else
+     python3 reco_plus.py \
+     --p_config "configs/reco_plus_cityscapes.yaml" \
+     --p_state_dict "$1"
+fi
